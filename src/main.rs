b4a395a34eef580ad0fd45559a8fb669d1118840
NoSequel <nosequel@protonmail.com>
2021-08-17 01:05:28 +0200

    add more package code

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ mod package;
 use package::Package;
 
 fn main() {
-    Package::new("hello!").execute_command();
+    let package = &mut Package::load_package(String::from("./"));
+
+    if let Some(package) = package {
+        package.execute_command();
+        package.store_package();
+    }
 }
--- src/package.rs
+++ src/package.rs
@@ -1,44 +1,95 @@
-use std::process::Command;
+use serde::{Deserialize, Serialize};
+use std::{
+    fs,
+    fs::File,
+    io::Write,
+    process::Command,
+};
 
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Package {
     name: String,
     version_id: String,
+    command: String,
+    path: String,
+
     print_command_output: bool,
-    command: String
 }
 
 impl Package {
-    pub fn new(name: &str) -> Self {
+    pub fn new(name: &str, path: &str) -> Self {
         Self {
             name: name.to_string(),
+            path: path.to_string(),
+
             version_id: "1.0".to_string(),
+            command: "ls".to_string(),
+
             print_command_output: true,
-            command: "dir".to_string()
+        }
+    }
+
+    pub fn load_package(path: String) -> Option<Self> {
+        let contents = fs::read_dir(path).unwrap();
+
+        for content in contents {
+            if let Ok(content) = content {
+                if content
+                    .file_name()
+                    .to_str()
+                    .map(|s| s.to_string())
+                    .unwrap()
+                    .ends_with(".package")
+                {
+                    return Some(
+                        serde_yaml::from_str(
+                            fs::read_to_string(content.path().as_os_str().to_str().unwrap())
+                                .unwrap()
+                                .as_str(),
+                        )
+                        .expect("Unable to read file."),
+                    );
+                }
+            }
+        }
+
+        return None;
+    }
+
+    pub fn store_package(&mut self) {
+        let file = &mut File::create(self.clone().path);
+
+        if let Ok(file) = file {
+            file.write_all(
+                serde_yaml::to_string(self)
+                    .expect("Unable to parse to YAML")
+                    .as_bytes(),
+            )
+            .unwrap();
         }
     }
 
     pub fn execute_command(&mut self) {
         let parameters = self.command.split(" ");
-        
+
         let mut command = "";
         let mut arguments: Vec<String> = vec![];
 
         for ele in parameters {
-            if command == ""  {
+            if command == "" {
                 command = ele;
             } else {
                 arguments.push(ele.to_string());
             }
         }
 
-
-        let command_result = Command::new(command)
-            .args(arguments)
-            .spawn();
+        let command_result = Command::new(command).args(arguments).output();
 
         if self.print_command_output {
             if let Ok(result) = command_result {
-                println!("{:?}", result);
+                println!("{:?}", String::from_utf8(result.stdout).unwrap());
+            } else {
+                println!("Unable to execute command.");
             }
         }
     }
